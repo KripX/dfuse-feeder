f9dd8dada295738565dd4964bb7c8c4c5a339916
Cesar Rodriguez <kesarr@gmail.com>
2018-10-20 13:47:19 +0200

    🚀

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,34 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
 
+const contracts = [
+  'decentwitter',
+  'nebulasystem',
+  'nebulaccount',
+  'nebulatokenn',
+  'nebulacoinnn',
+  'nebulatalked',
+  'nebulaskills',
+  'nebulajobbbb'
+];
+
 app.onopen = () => {
   console.log({ref: "app::open", message: "connection open"});
-  app.send(get_actions("decentwitter", "tweet", null, {
-    req_id: "decentwitter::tweet",
-    start_block: Number(process.env.START_BLOCK)
-  }));
+
+  for (const account of contracts) {
+    app.send(get_actions(account, null, null, { start_block: Number(process.env.START_BLOCK) }))
+  }
+
+  /* app.send(get_actions("decentwitter", "tweet", null, { req_id: "decentwitter::tweet", start_block: Number(process.env.START_BLOCK) })); */
 };
 
 app.onmessage = (message) => {
-  const tweets = parse_actions<Tweet>(message.data, "decentwitter::tweet");
+  //const tweets = parse_actions<Tweet>(message.data, "decentwitter::tweet");
+  const action = parse_actions(message.data);
 
-  if (tweets) {
-    console.log(tweets.data.trace.act);
-    backend.post(process.env.HTTP_HOST, tweets.data)
+  if (action) {
+    console.log(action.data.trace.act);
+    backend.post(process.env.HTTP_HOST, action.data)
       .catch(function (error) {
         console.log('http error: ' + error.response);
     });
