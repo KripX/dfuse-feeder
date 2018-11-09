9ccd8237f04fff5cb86f5a7e5d2ff8284c5e08b9
Cesar Rodriguez <kesarr@gmail.com>
2018-11-09 18:01:33 +0100

    🚀

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
-import {get_actions, parse_actions} from "eosws";
+import {
+  ActionTraceData,
+  createEoswsSocket,
+  EoswsClient,
+  ErrorData,
+  InboundMessage,
+  InboundMessageType
+} from "@dfuse/eosws-js";
 import * as Amqp from "amqp-ts";
 import WebSocket from "ws"
 
@@ -21,33 +28,37 @@ const contracts = [
 
 const connection = new Amqp.Connection(process.env.CLOUDAMQP_URL);
 const queue = connection.declareQueue('messages', {durable: true});
+const origin = process.env.ORIGIN;
+const socketFactory = () => new WebSocket(`wss://staging-mainnet.eos.dfuse.io/v1/stream?token=${process.env.API_TOKEN}`, {origin});
 
-function init() {
-  const origin = process.env.ORIGIN;
-  const app = new WebSocket(`wss://mainnet.eos.dfuse.io/v1/stream?token=${process.env.API_TOKEN}`, {origin});
+const onMessage = (message: InboundMessage<any>) => {
+  if (message.type === InboundMessageType.ACTION_TRACE) {
+    const actionTraceData = message.data as ActionTraceData<any>;
+    console.log(actionTraceData.trace.act);
+    const amqMessage = new Amqp.Message(JSON.stringify(actionTraceData.trace.act));
+    queue.send(amqMessage);
+    return;
+  }
+  if (message.type === InboundMessageType.ERROR) {
+    const error = message.data as ErrorData;
+    console.log(`Received ERROR: ${error.message} (${error.code})`, error.details);
+    return;
+  }
+};
 
-  app.onopen = () => {
-    console.log({ref: "app::open", message: "connection open"});
+const onClose = () => {
+  console.log({ref: "app", message: "connection closed"});
+};
 
-    for (const account of contracts) {
-      app.send(get_actions(account, null, null, {start_block: Number(process.env.START_BLOCK)}))
-    }
-  };
+const client = new EoswsClient(createEoswsSocket(socketFactory, {onClose, autoReconnect: true}));
+client.connect().then(() => {
+  console.log({ref: "app::open", message: "connection open"});
+  for (const account of contracts) {
+    client
+      .getActionTraces({account}, {start_block: Number(process.env.START_BLOCK)})
+      .onMessage(onMessage);
+  }
+}).catch((error) => {
+  console.log("ERROR: Unable to connect", error)
+});
 
-  app.onmessage = (message: any) => {
-    const action = parse_actions(message.data);
-
-    if (action) {
-      console.log(action.data.trace.act);
-      const message = new Amqp.Message(JSON.stringify(action));
-      queue.send(message);
-    }
-  };
-
-  app.onclose = () => {
-    console.log({ref: "app", message: "connection closed"});
-    setTimeout(init, 5000);
-  };
-}
-
-init();
