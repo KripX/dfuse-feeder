f10d326a20e78dee229b1cd2e4dd5570d0261a85
Cesar Rodriguez <kesarr@gmail.com>
2018-11-10 10:13:02 +0100

    🚀

--- src/server.ts
+++ src/server.ts
@@ -28,41 +28,51 @@ const contracts = [
 
 const connection = new Amqp.Connection(process.env.CLOUDAMQP_URL);
 const queue = connection.declareQueue('messages', {durable: true});
-const origin = process.env.ORIGIN;
-const socketFactory = () => new WebSocket(`wss://staging-mainnet.eos.dfuse.io/v1/stream?token=${process.env.API_TOKEN}`, {origin});
 
-const onMessage = (message: InboundMessage<any>) => {
-  if (message.type === InboundMessageType.ACTION_TRACE) {
-    const actionTraceData = message.data as ActionTraceData<any>;
-    console.log(actionTraceData.trace.act);
-    const amqMessage = new Amqp.Message(JSON.stringify(message));
-    queue.send(amqMessage);
-    return;
-  }
-  if (message.type === InboundMessageType.ERROR) {
-    const error = message.data as ErrorData;
-    console.log(`Received error: ${error.message} (${error.code})`, error.details);
-    return;
-  }
-};
+function init() {
+  const origin = process.env.ORIGIN;
+  const socketFactory = () => new WebSocket(`wss://staging-mainnet.eos.dfuse.io/v1/stream?token=${process.env.API_TOKEN}`, {origin});
 
-const onClose = () => {
-  console.log({ref: "app", message: "connection closed"});
-};
+  const onMessage = (message: InboundMessage<any>) => {
+    if (message.type === InboundMessageType.ACTION_TRACE) {
+      const actionTraceData = message.data as ActionTraceData<any>;
+      console.log(actionTraceData.trace.act);
+      const amqMessage = new Amqp.Message(JSON.stringify(message));
+      queue.send(amqMessage);
+      return;
+    }
+    if (message.type === InboundMessageType.ERROR) {
+      const error = message.data as ErrorData;
+      console.log(`Received error: ${error.message} (${error.code})`, error.details);
+      return;
+    }
+  };
 
-const onError = () => {
-  console.log({ref: "app", message: "error detected"});
-};
+  const onClose = () => {
+    console.log({ref: "app", message: "connection closed"});
+  };
 
-const client = new EoswsClient(createEoswsSocket(socketFactory, {onError, onClose, autoReconnect: true}));
-client.connect().then(() => {
-  console.log({ref: "app::open", message: "connection open"});
-  for (const account of contracts) {
-    client
-      .getActionTraces({account}, {start_block: Number(process.env.START_BLOCK)})
-      .onMessage(onMessage);
-  }
-}).catch((error) => {
-  console.log("error: Unable to connect", error)
-});
+  const onError = () => {
+    console.log({ref: "app", message: "error detected"});
+  };
 
+  const client = new EoswsClient(createEoswsSocket(socketFactory, {onError, onClose, autoReconnect: true}));
+  client.connect().then(() => {
+    console.log({ref: "app::open", message: "connection open"});
+    for (const account of contracts) {
+      client
+        .getActionTraces({account}, {start_block: Number(process.env.START_BLOCK)})
+        .onMessage(onMessage);
+    }
+  }).catch((error) => {
+    console.log("error: Unable to connect", error);
+  });
+}
+
+try {
+  init();
+}
+catch (err) {
+  console.log("error: Catch", err);
+  setTimeout(init, 5000);
+}
